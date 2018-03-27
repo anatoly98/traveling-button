8bca29b08b5d7d8690baf2bc203f32398318a0f2
Anatoly98 <32023104+anatoly98@users.noreply.github.com>
2018-03-28 00:31:27 +0300

    Add files via upload

---
+++ first_form/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace first_form
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.WindowText;
+            this.label1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(218, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Click button to exit.";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button
+            // 
+            this.button.Location = new System.Drawing.Point(227, 182);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(75, 23);
+            this.button.TabIndex = 1;
+            this.button.Text = "Close";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            this.button.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button_MouseMove_1);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(534, 411);
+            this.ControlBox = false;
+            this.Controls.Add(this.button);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximumSize = new System.Drawing.Size(550, 450);
+            this.MinimumSize = new System.Drawing.Size(550, 450);
+            this.Name = "Form";
+            this.Text = "useless program";
+            this.Load += new System.EventHandler(this.Form_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button;
+    }
+}
+
---
+++ first_form/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace first_form
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+       private Random r = new Random();
+
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+ 
+        private void Form_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_MouseMove_1(object sender, MouseEventArgs e)
+        {
+            if (Control.ModifierKeys == Keys.Control) return;
+
+            button.Location = new Point(r.Next(ClientRectangle.Width - 5), r.Next(ClientRectangle.Height - 5));
+        }
+    }
+}
